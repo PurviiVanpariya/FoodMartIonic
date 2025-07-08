2f7b87d8190da7ae2b28ad97c2897df2a330ad14
JuliSavalialhmr <js.hmrtech@gmail.com>
2025-07-08 09:39:09 +0530

    fixed issue of routes

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -1,9 +1,18 @@
-import { IonPage } from '@ionic/react'
-import React from 'react'
+import { IonPage } from '@ionic/react';
+import Header from '../header';
+import { IoAdd, IoMenu } from 'react-icons/io5';
 
 const Categories = () => {
   return (
     <IonPage>
+       <Header
+        icon={IoMenu}
+        cartIcon={IoAdd}
+        Heading='Categories'
+        cartClassName="after:hidden"
+        searchbar={false}
+        // onCartClick={onCartClick}
+      />
       categories
     </IonPage>
   )
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -11,6 +11,7 @@ import {
   IonIcon,
   IonLabel,
   IonItem,
+  useIonRouter,
 } from '@ionic/react';
 import { card, cart, clipboard, grid, heart, home, list, person, pricetag, location } from 'ionicons/icons';
 import { useState } from 'react';
@@ -28,21 +29,29 @@ type HeaderProps = {
   onCartClick?: any;
 };
 
-const listItem = [
-  { icon: home,path:"/home", label: 'Homepage' },
-  { icon: list,path:"/categories", label: 'Categories' },
-  { icon: grid,path:"/home", label: 'Shop List' },
-  { icon: pricetag,path:"/home", label: 'Single Product' },
-  { icon: cart,path:"/home", label: 'Shopping Cart' },
-  { icon: card,path:"/home", label: 'Checkout' },
-  { icon: person,path:"/home", label: 'My Profile' },
-  { icon: location,path:"/home", label: 'My Address' },
-  { icon: clipboard,path:"/home", label: 'Order List' },
-  { icon: heart,path:"/home", label: 'About Us' }
-]
 const Header = ({ icon, SubHeading, Heading, cartIcon, searchbar = false, cartClassName, onCartClick }: HeaderProps) => {
 
   const [searchTerm,] = useState("");
+
+  const listItem = [
+    { icon: home, path: "/home", label: 'Homepage' },
+    { icon: list, path: "/categories", label: 'Categories' },
+    { icon: grid, path: "/home", label: 'Shop List' },
+    { icon: pricetag, path: "/home", label: 'Single Product' },
+    { icon: cart, path: "/home", label: 'Shopping Cart' },
+    { icon: card, path: "/home", label: 'Checkout' },
+    { icon: person, path: "/home", label: 'My Profile' },
+    { icon: location, path: "/home", label: 'My Address' },
+    { icon: clipboard, path: "/home", label: 'Order List' },
+    { icon: heart, path: "/home", label: 'About Us' }
+  ]
+
+  const history = useIonRouter();
+
+  const handleNavigation = (path: string) => {
+    history.push(path);
+  };
+
 
   return (
     <>
@@ -59,30 +68,22 @@ const Header = ({ icon, SubHeading, Heading, cartIcon, searchbar = false, cartCl
           </IonRow>
           <IonList>
             {listItem.map((item, index) => (
-              <Link
-              to={item.path}
-              key={index}
-              className="text-decoration-none"
-              >
               <IonItem
                 key={index}
-                onClick={() => {item.path}}
-                lines="none"
                 button
+                onClick={() => handleNavigation(item.path)}
+                lines="none"
                 className="text-[#86888F] listMenu hover:bg-zinc-100 transition-all duration-500 rounded-md"
               >
                 <IonIcon icon={typeof item.icon === 'string' ? item.icon : undefined} slot="start" className="text-xl" />
                 <IonLabel className='font-semibold'>{item.label}</IonLabel>
               </IonItem>
-              </Link>
             ))}
           </IonList>
         </IonContent>
       </IonMenu>
       <IonHeader id='main-content' className="relative max-h-[130px] h-fit size-full bg-[#003049] py-2 px-3 flex flex-wrap gap-x-5 !items-center !shadow-none">
-        {/* {icon && ( */}
-          <IonMenuButton className='text-white'></IonMenuButton>
-         {/* )} */}
+        <IonMenuButton className='text-white'></IonMenuButton>
         <IonRow className="flex flex-col">
           <IonText className="text-white text-xs">{SubHeading}</IonText>
           <IonText className="text-white text-base font-semibold">{Heading}</IonText>
